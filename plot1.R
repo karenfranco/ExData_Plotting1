938d26c754d9e3fbc7a5f5003b6f47fc9cc37dcf
Karen Franco <kfranco@live.com>
2015-12-10 16:06:22 -0800

    Exploratory Data Analysis Course Project 1
    Plot 1 and 2 R scripts and PNG files

---
+++ plot1.R
@@ -0,0 +1,15 @@
+## Read in data file
+plot1 <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+
+## Subset data to just show the dates 2007-02-01 and 2007-02-02
+plot1_subset <- subset(plot1, as.Date(strptime(Date, "%d/%m/%Y")) == as.Date("2007-02-01") | as.Date(strptime(Date, "%d/%m/%Y")) == as.Date("2007-02-02"))
+
+## Convert Global_active_power from factor to numeric type
+gap <- as.numeric(levels(plot1_subset$Global_active_power))[as.integer(plot1_subset$Global_active_power)]
+
+## Draw Plot 1
+hist(gap, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+
+## Save to PNG file
+dev.copy(png, "plot1.png")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,19 @@
+## Read in data file
+plot2 <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+
+## Subset data to just show the dates 2007-02-01 and 2007-02-02
+plot2_subset <- subset(plot2, as.Date(strptime(Date, "%d/%m/%Y")) == as.Date("2007-02-01") | as.Date(strptime(Date, "%d/%m/%Y")) == as.Date("2007-02-02"))
+
+## Convert Global_active_power from factor to numeric type
+gap <- as.numeric(levels(plot2_subset$Global_active_power))[as.integer(plot2_subset$Global_active_power)]
+
+## Paste Date and Time together into one entity
+dt <- paste(plot2_subset$Date, plot2_subset$Time)
+dt2 <- strptime(dt, "%d/%m/%Y %H:%M:%S")
+
+## Draw Plot 2
+plot(dt2, gap, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+## Save to PNG file
+dev.copy(png, "plot2.png")
+dev.off()
